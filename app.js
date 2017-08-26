6c027eeea0f24d54e87008fd67b8f47ec9b8fdb8
Rauno <rauno@thorgate.eu>
2017-08-26 15:16:07 +0300

    ..

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
-'use strict';
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express();
-
-app.listen(process.env.PORT);
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
-
-// Process application/json
 app.use(bodyParser.json());
 
+app.listen(process.env.PORT);
 
 app.get('/hello', function (req, res) {
     res.send('hello world');
 });
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === 'poop') {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
     }
-    res.send('Error, wrong token')
-});
-
-// Spin up the server
-app.listen(app.get('port'), function () {
-    console.log('running on port', app.get('port'))
 });
