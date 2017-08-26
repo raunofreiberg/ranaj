dbda51390a3b31f51f1b2ef6344a7664f4d73969
Rauno <rauno@thorgate.eu>
2017-08-26 15:26:16 +0300

    no slash

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.get('/hello', function (req, res) {
     res.send('hello world');
 });
 
-app.get('/webhook/', function (req, res) {
+app.get('/webhook', function (req, res) {
 	if (req.query['hub.verify_token'] === 'poop') {
 		res.send(req.query['hub.challenge']);
-        console.log('done')
+        console.log('done');
 	}
 	res.send('Error, wrong token');
     console.log('fuck');
