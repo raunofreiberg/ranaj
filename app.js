fba121c2e41157cae2242930a5cb12160dc55a7f
Rauno <rauno@thorgate.eu>
2017-08-26 15:18:24 +0300

    asd

--- app.js
+++ app.js
@@ -10,13 +10,11 @@ app.get('/hello', function (req, res) {
     res.send('hello world');
 });
 
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === 'poop') {
-        console.log("Validating webhook");
-        res.status(200).send(req.query['hub.challenge']);
-    } else {
-        console.error("Failed validation. Make sure the validation tokens match.");
-        res.sendStatus(403);
-    }
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'poop') {
+		res.send(req.query['hub.challenge']);
+        console.log('done')
+	}
+	res.send('Error, wrong token');
+    console.log('fuck');
 });
