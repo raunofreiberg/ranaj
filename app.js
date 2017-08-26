4c2127d78215926ea883008d4909764a0bff5438
Rauno <rauno@thorgate.eu>
2017-08-26 15:32:22 +0300

    pls

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/webhook', function (req, res) {
     console.log('fuck');
 });
 
-app.post('/webhook/', function (req, res) {
+app.post('/webhook', function (req, res) {
     let messaging_events = req.body.entry[0].messaging;
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i];
