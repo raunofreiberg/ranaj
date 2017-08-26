04549bddf8190c9683f5db21d526ef30988cd9e5
Rauno <rauno@thorgate.eu>
2017-08-26 14:23:50 +0300

    npm script

--- app.js
+++ app.js
@@ -9,6 +9,4 @@ app.post('/webhook', function (req, res) {
     res.send('hello !!!!!');
 });
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
-});
+app.listen(process.env.PORT);
