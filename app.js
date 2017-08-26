371073e10e95f0a45444efe9a2098d84b34e83cc
Rauno <rauno@thorgate.eu>
2017-08-26 16:23:46 +0300

    random meme

--- app.js
+++ app.js
@@ -12,122 +12,130 @@ app.get('/hello', function (req, res) {
 });
 
 app.get('/webhook', function (req, res) {
-	if (req.query['hub.verify_token'] === 'beniz') {
-		res.send(req.query['hub.challenge']);
+    if (req.query['hub.verify_token'] === 'beniz') {
+        res.send(req.query['hub.challenge']);
         console.log('done');
-	}
-	res.send('Error, wrong token');
+    }
+    res.send('Error, wrong token');
     console.log('fuck');
 });
 
 app.post('/webhook', function (req, res) {
-  let data = req.body;
+    let data = req.body;
 
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      let pageID = entry.id;
-      let timeOfEvent = entry.time;
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function (entry) {
+            let pageID = entry.id;
+            let timeOfEvent = entry.time;
 
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
+            // Iterate over each messaging event
+            entry.messaging.forEach(function (event) {
+                if (event.message) {
+                    receivedMessage(event);
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
 
-    res.sendStatus(200);
-  }
+        res.sendStatus(200);
+    }
 });
 
 function receivedMessage(event) {
-  let senderID = event.sender.id;
-  let recipientID = event.recipient.id;
-  let timeOfMessage = event.timestamp;
-  let message = event.message;
+    let senderID = event.sender.id;
+    let recipientID = event.recipient.id;
+    let timeOfMessage = event.timestamp;
+    let message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:",
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
+    console.log("Received message for user %d and page %d at %d with message:",
+        senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
 
-  let messageId = message.mid;
+    let messageId = message.mid;
 
-  let messageText = message.text;
-  let messageAttachments = message.attachments;
+    let messageText = message.text;
+    let messageAttachments = message.attachments;
 
-  if (messageText) {
+    if (messageText) {
 
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'meme':
-        sendMemeMessage(senderID);
-        break;
+        // If we receive a text message, check to see if it matches a keyword
+        // and send back the example. Otherwise, just echo the text we received.
+        switch (messageText) {
+            case 'meme':
+                sendMemeMessage(senderID);
+                break;
 
-      default:
-        sendTextMessage(senderID, messageText);
+            default:
+                sendTextMessage(senderID, messageText);
+        }
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
     }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
 }
 
 function sendTextMessage(recipientId, messageText) {
-  let messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
+    let messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: messageText
+        }
+    };
 
-  callSendAPI(messageData);
+    callSendAPI(messageData);
 }
 
 function sendMemeMessage(recipientId) {
-  let messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "image",
-        payload: {
-            url: "http://fillmurray.com/250/250"
-        }
-      }
-    }
-  };
+    const memes = [
+        'http://i.imgur.com/reW77sh.gifv',
+        'http://i.imgur.com/N9puck0.png',
+        'http://i.imgur.com/e40YXcs.jpg',
+    ];
 
-  callSendAPI(messageData);
+    let memeToBeSent = memes[Math.floor(Math.random()*memes.length)];
+
+    let messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "image",
+                payload: {
+                    url: memeToBeSent;
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
 }
 
 const token = 'EAAD2EIPvxu0BAPYULJz85kD8ZC0faU2u3DqI4Q2v0hm3gw4eSk19LsLeIZAutVcMgaGbHknT9ZCZBdpZCpk5ZALHjCd8uWvXhObKdfMWhjifDdRlGQg7dObKXkYGKPme32ufTwhdKTk1Fw5b7VM0yE13JNWZBZBCRnnvBZAF7ZBkT5YNMvdYRZAY4wO';
 
 function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: token },
-    method: 'POST',
-    json: messageData
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: messageData
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      let recipientId = body.recipient_id;
-      let messageId = body.message_id;
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            let recipientId = body.recipient_id;
+            let messageId = body.message_id;
 
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });
+            console.log("Successfully sent generic message with id %s to recipient %s",
+                messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
+    });
 }
