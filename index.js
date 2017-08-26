85c6d43e690bf7a3f87db71002b157eed12e29f5
Rauno <rauno@thorgate.eu>
2017-08-26 14:10:36 +0300

    add webhooks

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/hello', function(req, res) {
+    res.send('hello world');
+});
+
+app.post('/webhook', function(req, res) {
+    res.send('hello !!!!!');
+});
